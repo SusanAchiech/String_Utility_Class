2447f6ed72c1cc9eeb9828a23eb55100b141a7ff
Susan Achiech <127722179+SusanAchiech@users.noreply.github.com>
2024-05-28 11:23:30 +0300

    creating an array of spells and checking if the player knows the spells

--- String_Utility_Class_Assessment/Player.h
+++ String_Utility_Class_Assessment/Player.h
@@ -1,12 +1,33 @@
 #pragma once
 #include <vector>
 #include "String.h"
+#include <iostream>
 class Player
 {
 public:
-	Player();
+	Player()
+	{
+		//creating  an array of spell names
+		spells = { "Eclipse", "Frostbite", "Levitate", "Mirage", "Petrify", "Teleport", "Thunderwave", "Vortext" };
+	}
 	~Player();
-	bool FindSpell(String spellName);
+	//performing a binary function to check the spells
+	bool FindSpell(String spellName)
+	{
+		auto it = std::lower_bound(spells.begin(), spells.end(), spell);
+
+		//checking if the player knows the spell
+		if (it != spells.end() && *it == spell)
+		{
+			std::cout << "'You know the spell '" << spell.getString() << std::endl;
+			return true;
+		}
+		else
+		{
+			std::cout << "'You do not know the spell '" << spell.getString() << std::endl;
+			return false;
+		}
+	}
 
 private:
 	std::vector<String> spells;
