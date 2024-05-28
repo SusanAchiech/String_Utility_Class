a5e45bf4ca61485067130d42e91171f089255e3c
Susan Achiech <127722179+SusanAchiech@users.noreply.github.com>
2024-05-28 11:39:22 +0300

    Updating the spell.h file

--- String_Utility_Class_Assessment/Spell.h
+++ String_Utility_Class_Assessment/Spell.h
@@ -1,13 +1,22 @@
 #pragma once
 #include "String.h"
+#include <iostream>
 
 class Spell
 {
 public:
-	Spell();
+	//initializing name as empty and damage to 0
+	Spell(): name(""), damage(0){}
 	~Spell()
-	void Cast();
-	bool Compare(Spell& a, Spell& b) const;
+		void Cast()
+	{
+		std::cout << "You cast " << name.getString() << " dealing" << damage << " damage!" << std::endl;
+	}
+	//comparing the two spell objects
+	bool Compare(const Spell& a, const Spell& b) const
+	{
+		return a.name < b.name;
+	}
 
 private:
 	String name;
