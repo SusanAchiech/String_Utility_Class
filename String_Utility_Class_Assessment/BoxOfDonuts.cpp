2f4180a2fb712e8d3c037668540a31fa84ebbbdf
Susan Achiech <127722179+SusanAchiech@users.noreply.github.com>
2024-05-28 08:50:57 +0300

    Override and use functions in the  BoxOfDonuts header file

---
+++ String_Utility_Class_Assessment/BoxOfDonuts.cpp
@@ -0,0 +1,2 @@
+#include "BoxOfDonuts.h"
+
---
+++ String_Utility_Class_Assessment/BoxOfDonuts.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "Item.h"
+
+class BoxOfDonuts: public Item 
+{
+public:
+	BoxOfDonuts();
+	~BoxOfDonuts();
+	void Use() override;
+	void Description() override;
+private:
+	int donuts;
+}
