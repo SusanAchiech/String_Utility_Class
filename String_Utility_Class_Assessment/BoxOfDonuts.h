ac06c3539da8152d49c875e24d8e07a81277d6d1
Susan Achiech <127722179+SusanAchiech@users.noreply.github.com>
2024-05-28 10:49:33 +0300

    checking the if the remaining number of dunots have been eaten

--- String_Utility_Class_Assessment/BoxOfDonuts.h
+++ String_Utility_Class_Assessment/BoxOfDonuts.h
@@ -19,8 +19,16 @@ public:
 		}
 	}
 	void Use() override
-	{
-
+	{	//checking the if the remaining number of dunots have been eaten
+		if (donuts > 0)
+		{
+			donuts--;
+			std::cout << "The remaining " << donuts << " donuts have been eaten.\n";
+		}
+		else
+		{
+			std::cout << "You cannot eat what you do not have!\n";
+		}
 	}
 private:
 	int donuts;
