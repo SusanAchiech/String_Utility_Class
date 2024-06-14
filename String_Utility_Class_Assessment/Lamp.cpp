240a04d629a321a54717faf0523e62817fd6be9b
Susan Achiech <127722179+SusanAchiech@users.noreply.github.com>
2024-06-14 11:55:20 +0300

    lamp.h to lamp.cpp

--- String_Utility_Class_Assessment/Lamp.cpp
+++ String_Utility_Class_Assessment/Lamp.cpp
@@ -1,5 +1,31 @@
 #include "Lamp.h"
-Lamp::Lamp()
-{
 
+// Constructor definition
+Lamp::Lamp() : isOn(false) {}
+
+// Destructor definition
+Lamp::~Lamp() {}
+
+// Use method definition
+void Lamp::Use() {
+    if (isOn) {
+        std::cout << "You turn off the lamp and there is darkness." << std::endl;
+        isOn = false;
+    }
+    else {
+        std::cout << "You turn on the lamp and there is light." << std::endl;
+        isOn = true;
+    }
+}
+
+// Description method definition
+void Lamp::Description() {
+    std::string desc = "A glass lamp";
+    if (isOn) {
+        desc += " that is turned on";
+    }
+    else {
+        desc += " that is turned off";
+    }
+    std::cout << desc << std::endl;
 }
--- String_Utility_Class_Assessment/Lamp.h
+++ String_Utility_Class_Assessment/Lamp.h
@@ -1,43 +1,15 @@
 #pragma once
 #include "Item.h"
 #include <iostream>
-class Lamp: public Item
+
+class Lamp : public Item
 {
-public: 
-	Lamp() : isOn(false)
-	{
-	}
-		~Lamp();
-
-	void Use()
-	{
-		if (isOn)
-		{
-			std::cout << "You turn on lamp and there is light." << std::endl;
-		
-		}
-		else
-		{
-			std::cout << "You turn off lanp and there is darkness" << std::endl;
-			
-		}
-	}
-	void Description()
-
-	{
-		std::string desc = "A glass lamp";
-		if (isOn)
-		{
-			desc += " that is turned on";
-		}
-		else
-		{
-			desc += " that is turned off";
-		}
-		std::cout << desc << std::endl;
-	}
+public:
+    Lamp();
+    ~Lamp();
+    void Use() override;
+    void Description() override;
 
 private:
-	bool isOn;
+    bool isOn;
 };
-
