70dd073d16545dc8239b447b2aaf95163812e843
Susan Achiech <127722179+SusanAchiech@users.noreply.github.com>
2024-06-14 11:50:46 +0300

    tranfering codes from BoxOfDonuts header file to cpp file

--- String_Utility_Class_Assessment/BoxOfDonuts.cpp
+++ String_Utility_Class_Assessment/BoxOfDonuts.cpp
@@ -1,2 +1,36 @@
 #include "BoxOfDonuts.h"
 
+// Constructor definition
+BoxOfDonuts::BoxOfDonuts() : donuts(12) {}
+
+// Destructor definition
+BoxOfDonuts::~BoxOfDonuts() {}
+
+// Description method definition
+void BoxOfDonuts::Description()
+{
+    // Checking the number of donuts left
+    if (donuts > 0)
+    {
+        std::cout << "This box of donuts has " << donuts << " left.\n";
+    }
+    else
+    {
+        std::cout << "The box is empty.\n";
+    }
+}
+
+// Use method definition
+void BoxOfDonuts::Use()
+{
+    // Checking if the remaining number of donuts have been eaten
+    if (donuts > 0)
+    {
+        donuts--;
+        std::cout << "The remaining " << donuts << " donuts have been eaten.\n";
+    }
+    else
+    {
+        std::cout << "You cannot eat what you do not have!\n";
+    }
+}
--- String_Utility_Class_Assessment/BoxOfDonuts.h
+++ String_Utility_Class_Assessment/BoxOfDonuts.h
@@ -5,32 +5,11 @@
 class BoxOfDonuts : public Item
 {
 public:
-	BoxOfDonuts();
-	~BoxOfDonuts();
-	void Description() override
-	{
-		//checking the number of donuts left
-		if (donuts > 0)
-		{
-			std::cout << "This box of donuts has " << donuts << "left.\n";
-		}
-		else
-		{
-			std::cout << "The box is empty.\n";
-		}
-	}
-	void Use() override
-	{	//checking the if the remaining number of dunots have been eaten
-		if (donuts > 0)
-		{
-			donuts--;
-			std::cout << "The remaining " << donuts << " donuts have been eaten.\n";
-		}
-		else
-		{
-			std::cout << "You cannot eat what you do not have!\n";
-		}
-	}
+    BoxOfDonuts();
+    ~BoxOfDonuts();
+    void Description() override;
+    void Use() override;
+
 private:
-	int donuts;
+    int donuts;
 };
